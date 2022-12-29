63742e20ba4043c57b2f68e0fd1552b2897b46b1
David Chardon <syhnes@gmail.com>
2022-12-29 12:27:44 +0100

    supression du fichier liste

--- Listes.py
+++
@@ -1,7 +0,0 @@
-liste = ['Python', '5', 'py', '4', 'PHP', '8']
-# affichage de la liste
-print("liste originale : " + str(liste))
-#remplacement
-res = [elem.replace('4', '5') for elem in liste]
-# print result
-print("liste apres replacement : " + str(res))
