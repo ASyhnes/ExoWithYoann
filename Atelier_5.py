6e91b611eb59f78bdf4367307221a5c4c5a9c32e
David Chardon <syhnes@gmail.com>
2023-01-06 12:39:55 +0100

    ajout atelier5 et go=> exo 7

---
+++ Atelier_5.py
@@ -0,0 +1,139 @@
+#Exercice 1 (clé) :
+#) Créer une fonction qui prend en argument 2 nombres et retourne leur somme multiplié
+#par 2.
+#2) Créer une fonction qui retourne la moyenne de 2 nombres.
+
+def function(n):
+    somme = n * 2
+    print("le nombre multiplié par 2 donne :",somme)
+
+n = int(input("Choisissez un nombre : "))
+function(n)
+
+
+#Exercice 2 (clé) :
+#1) Créer une fonction qui prend en entrée 2 nombres et qui retourne le plus grand de ces 2
+#nombres.
+#2) Créer une fonction qui prend en entrée 2 nombres et qui retourne le plus petit de ces 2
+#nombres.
+#3) Créer un programme qui demande à l’utilisateur de saisir un premier nombre puis un
+#deuxième nombre et qui retourne le plus grand et le plus petit de ces nombres
+#4) Modifier le programme pour que celui-ci prenne en compte le cas où les nombres sont
+#égaux.
+
+def function_A(n1,n2):
+    f = "le plus grand nombre est :"
+    if n1>n2:
+        print(f)
+        resulta =  n1
+    elif n2>n1:
+        print(f)
+        resulta =  n2
+    else:
+        resulta = "les nombres sont égaux"
+    return resulta
+
+print(function_A(int(input("Choisissez un nombre : ")),int(input("Choisissez un deuxiéme nombre : "))))
+
+
+#Exercice 3 (clé) :
+#1) Créer une fonction permettant de calculer l’aire d’un triangle
+#2) Créer une fonction retournant la valeur de l'hypoténuse d’un triangle rectangle en
+#prenant en entrée les 2 plus petits côtés du triangle (théorème de pythagore).
+
+def aire_triangle(b,h):
+    aire = b * h /2
+    return aire
+
+print("la surface du triangle est de :",aire_triangle(int(input("Choisissez un nombre pour la base du triangle : ")), int(input("Choisissez un nombre pour la hauteur du triangle : "))))
+
+def hypot_triangle(a,b):
+    hypot = (a ** 2 + b ** 2) ** 0.5
+    return hypot
+
+print("l'hypoténuse du triangle est de :",hypot_triangle(int(input("Choisissez un nombre pour le plus petit coté : ")), int(input("Choisissez un nombre pour le plus grand coté : "))))
+
+
+#Exercice 4 (clé) :
+#1) Créer une fonction qui retourne la plus grande valeur d’une liste d’entiers
+#Exemple : [1, 8, 4, 2] => 8
+
+liste = []
+n = int(input("combien voulez vous choisir d'entier pour la liste a analiser? "))
+for e in range(0,n):
+    print("choisissez votre entier numéro ", e + 1)
+    z = int(input())
+    liste.append(z)
+
+def plus_grand(liste):
+    n = (max(liste))
+    return (n)
+
+print("l'entier le plus grand est :",(plus_grand(liste)))
+
+#2) Créer une fonction qui retourne la somme des valeurs d’une liste d’entiers
+#Exemple : [1, 8, 4, 2] => 15
+
+def somme_valeurs(liste):
+    s = sum(liste)
+    return s
+
+print("la somme de tout les entiers de la liste est :",(somme_valeurs(liste)))
+
+#3) Créer une fonction qui retourne la valeure moyenne d’une liste d’entiers (pour aller plus
+#vite vous avez le droit d’utiliser la fonction de la question 2)
+#Exemple : [1, 8, 4, 2] => 3.75
+def moyenne_valeurs(liste):
+    m = sum(liste)/len(liste)
+    return m
+
+print("la moyenne de tout les entiers de la liste est :",(moyenne_valeurs(liste)))
+
+#Exercice 5 (secondaire) :
+#1) Créer une fonction qui prend en entrée un nombre et qui retourne un booléen True si ce
+#nombre est pair (indice : pour vérifier si un nombre est pair on utilise le modulo :
+#nbr % 2 == 0)
+
+n = int(input("entrez un nombre :"))
+
+def nombre_pair(n):
+    return (n % 2) == 0
+
+print("votre nombre est pair:", nombre_pair(n))
+
+
+#2) Créer une fonction qui prend en entrée un nombre et qui retourne un booléen True si ce
+#nombre est impair (indice : pour vérifier si un nombre est pair on utilise le modulo :
+#nbr % 2 == 1)
+
+def nombre_impair(n):
+    return (n % 2) == 1
+
+print("votre nombre est impair:", nombre_impair(n))
+
+#3) Vérifier le bon fonctionnement de ces fonctions
+
+#Exercice 6 (secondaire) :
+#1) Créer une fonction qui compte le nombre de caractères d’un mot.
+
+n = str(input("entrez un mot :"))
+
+def nb_chara_mot(n):
+    long_mot = len(n)
+    return long_mot
+
+print("il y a ", nb_chara_mot(n), " charactéres dans votre mot.")
+
+#2) Créer une fonction qui compte le nombre de mots d’une ligne
+
+ph = str(input("entrez une phrase :"))
+
+def nb_chara_phrase(ph):
+    long_phrase = len(ph)
+    return long_phrase
+
+print("il y a ", nb_chara_phrase(ph), " charactéres dans votre phrase.")
+
+#Exercice 7 (secondaire) :
+#1) Créer un programme qui prend en entrée un nombre et qui retourne une liste de nombre
+#entier aléatoire dont l’addition vaut le nombre en entrée.
--- debug.py
+++ debug.py
@@ -1,8 +1,7 @@
-def print_multiforce(n, p):
-    for i in range(p):
-        for j in range(1, (n * 2) + 1, 2):
-            print((j * "^").center(n * 2))
+ph = str(input("entrez une phrase :"))
 
-n = int(input("Choisissez la hauteur d'un triangle : "))
-p = int(input("Choisissez la hauteur de votre multiforce : "))
-print_multiforce(n, p)
+def nb_chara_phrase(ph):
+    long_phrase = len(ph)
+    return long_phrase
+
+print("il y a ", nb_chara_phrase(ph), " charactéres dans votre phrase.")
