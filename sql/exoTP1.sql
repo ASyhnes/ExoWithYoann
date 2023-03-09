246662e4588d8cde0cc352addb7a1b7a99e8e5c5
Asyhnes <ASyhnes>
2023-03-09 07:04:28 +0100

    table de donné SQL ajouté: région

--- sql/exoTP1.sql
+++ sql/exoTP1.sql
@@ -1,5 +1,7 @@
-CREATE TABLE Region (
-    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    Region_ID CHAR(2) NOT NULL,
-    Region_NAME VARCHAR(25)
+CREATE TABLE Departements (
+	 ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	 Departement_ID INT NOT NULL,
+	 Departement_Name VARCHAR(30) NOT NULL,
+	 Manager_ID INT,
+	 Location_ID INT
 );
