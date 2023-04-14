75303a7db3a2d06f380ed817806abe3821aedf59
Asyhnes <ASyhnes>
2023-04-14 15:05:20 +0200

    add sql requette region job

--- sql/requettes_region_job.sql
+++ sql/requettes_region_job.sql
@@ -1,11 +1,21 @@
+/*test et bidouille*/
+
 SELECT first_name, job_tittle FROM employees JOIN jobs ON employees.job_id = jobs.job_id;
 SELECT COUNT(*) FROM departments ;
 SELECT MIN(min_salary) FROM jobs ;
 SELECT MAX(max_salary) FROM jobs ;
 SELECT AVG((min_salary + max_salary) / 2) AS avg_salary FROM jobs;
+SELECT postal_code, city FROM locations ORDER BY postal_code ASC;
+SELECT * FROM employees ORDER BY last_name DESC;
 
---Trier les résultats par une colonne particulière
---SELECT <column_name>,<column_name_2>,… FROM <table> ORDER BY <column_name> ASC
---ASC permet de trier dans l’ordre croissant
---SELECT <column_name>,<column_name_2>,… FROM <table> ORDER BY <column_name> DESC
---DESC permet de trier dans l’ordre décroissant
+/*TP3 
+
+Partie TP – Indiquer le nombre de ligne obtenus avec la requête utilisée*/
+
+
+/*1. Afficher toutes les régions>>>>>> 4 rows*/ 
+SELECT * FROM regions;
+/*2. Afficher tous les pays>>>>>> 25 rows*/
+SELECT * FROM countries;
+/*3. Afficher uniquement les noms des pays>>>>>> 25 rows*/
+SELECT country_name FROM countries;
